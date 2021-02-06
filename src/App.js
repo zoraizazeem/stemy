9a6b92d74e250e67165fe3b19b3611bb6c4003a1
Zoraiz <zoraiz68@gmail.com>
2021-02-06 17:27:40 +0000

    added various npm packages, made headway on database addition, still yet incomplete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@
 import './App.css';
 import { Component } from 'react';
 import { MyHeader } from './Header';
+const Datastore = require('nedb');
 
+const database = new Datastore({ filename: "./database.db", autoload: true });
+database.loadDatabase();
+database.insert({ name: 'Tester', status: '😇'})
 
 
 class App extends Component {
